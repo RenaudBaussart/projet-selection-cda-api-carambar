dbeed5feccca2b5a98ef157d179a7ecb6a9fc638
Hugo-walando <hugowalando@gmail.com>
2024-06-09 21:25:00 +0200

    fix: fix random routes

--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {
-    createJoke,
-    updateJoke,
-    deleteJoke,
-    getAllJokes,
-    getJokeById,
-    getRandomJoke,
+  createJoke,
+  updateJoke,
+  deleteJoke,
+  getAllJokes,
+  getRandomJoke,
+  getJokeById,
 } = require('../controllers/jokeController');
 
 const router = express.Router();
@@ -18,8 +18,8 @@ router.delete('/:id', deleteJoke);
 
 router.get('/', getAllJokes);
 
-router.get('/:id', getJokeById);
-
 router.get('/random', getRandomJoke);
+
+router.get('/:id', getJokeById);
 
 module.exports = router;
