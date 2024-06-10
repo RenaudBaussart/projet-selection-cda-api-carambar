baf810fefd62c0bcf5560f5500c59abe8fb85202
renaudbaussart <renaud.baussart@proton.me>
2024-06-10 13:01:19 +0200

    feat: add cors to the dependencies and setting it up

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const setupSwagger = require('./swagger/swagger');
 const jokeRoutes = require('./routes/jokeRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var cors = require('cors')
 
 // Middleware to parse json
 app.use(express.json());
+
+//midelware to enable cors
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 // Configure Swagger
 setupSwagger(app);
